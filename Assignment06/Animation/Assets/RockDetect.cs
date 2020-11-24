cbd19485a3423d0ac4c1188980e94beb215abcd5
Sun-Yuhe <631463228@qq.com>
2020-11-24 21:13:43 +0800

    Assignment06 first commit

---
+++ Assignment06/Animation/Assets/RockDetect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDetect : MonoBehaviour
+{
+    public bool nearRock = false;
+
+    public void SetNearRock()
+    {
+        nearRock = true;
+        Debug.Log(nearRock);
+    }
+
+    public void SetAwayRock()
+    {
+        nearRock = false;
+        Debug.Log(nearRock);
+    }
+
+
+        
+
+}
